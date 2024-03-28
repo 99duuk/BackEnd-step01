873bcbe1d5a6e8bada3efa2894eb7c7b34bc6f8f
99duuk <99duuk@gmail.com>
2024-03-28 17:39:24 +0900

    setter초기화로 사용한다. 그래서 setter의 리턴값을 this로 해서 연속적인 chain 호출이 가능하게 한다.

--- _23_Vo_Dto/src/main/java/spms/vo/Member.java
+++ _23_Vo_Dto/src/main/java/spms/vo/Member.java
@@ -22,38 +22,44 @@ public class Member {
 	public int getNo() {
 		return no;
 	}
-	public void setNo(int no) {
+	public Member setNo(int no) {
 		this.no = no;
+		return this;
 	}
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
+	public Member setName(String name) {
 		this.name = name;
+		return this;
 	}
 	public String getEmail() {
 		return email;
 	}
-	public void setEmail(String email) {
+	public Member setEmail(String email) {
 		this.email = email;
+		return this;
 	}
 	public String getPassword() {
 		return password;
 	}
-	public void setPassword(String password) {
+	public Member setPassword(String password) {
 		this.password = password;
+		return this;
 	}
 	public Date getCreateDate() {
 		return createDate;
 	}
-	public void setCreateDate(Date createDate) {
+	public Member setCreateDate(Date createDate) {
 		this.createDate = createDate;
+		return this;
 	}
 	public Date getModifiedDate() {
 		return modifiedDate;
 	}
-	public void setModifiedDate(Date modifiedDate) {
+	public Member setModifiedDate(Date modifiedDate) {
 		this.modifiedDate = modifiedDate;
+		return this;
 	}
 	public Member(int no, String name, String email, String password, Date createDate, Date modifiedDate) {
 		super();
